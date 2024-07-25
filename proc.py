248a68df42762754034b77ceaa40356c392257fe
void0895 <164507313+void0895@users.noreply.github.com>
2024-07-25 23:08:17 +0530

    Add files via upload

--- proc.py
+++ proc.py
@@ -1,11 +1,10 @@
 import requests
-
+from time import sleep
 
 # url
 auth_url = "https://api.agiex.org/auth/connect"
 info_url = "https://oex-hub-app-api-dot-elite-crossbar-345112.uc.r.appspot.com/mission/get/mission"
 reward_url = "https://api.agiex.org/avatar/getReward"
-
 
 
 def auth(data):
@@ -14,8 +13,9 @@ def auth(data):
         "Content-Type": "application/json;charset=UTF-8",
         "User-Agent": "okhttp/4.9.2"
     }
-    r =  requests.post(auth_url, data=data, headers=headers)
+    r = requests.post(auth_url, data=data, headers=headers)
     return r.json().get('token')
+
 
 def info(data):
     headers = {
@@ -23,8 +23,9 @@ def info(data):
         "Content-Type": "application/json;charset=UTF-8",
         "User-Agent": "okhttp/4.9.2"
     }
-    r =  requests.post(info_url, data=data, headers=headers)
+    r = requests.post(info_url, data=data, headers=headers)
     return r.json().get('doc', {})
+
 
 def reward(token):
     headers = {
@@ -35,11 +36,11 @@ def reward(token):
         "Accept-Encoding": "gzip"
     }
     data = '{"aid":1480}'
-    r =  requests.post(reward_url, data=data, headers=headers)
+    r = requests.post(reward_url, data=data, headers=headers)
     if r.json().get('error'):
         print("skipping")
+    elif r.status_code != 200:
+        sleep(30)
+        reward(token)
     else:
         print("success")
-
-
-
