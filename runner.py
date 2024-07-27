cc78e775590c98dda9e834b807c1ba7a26e20de3
void0895 <164507313+void0895@users.noreply.github.com>
2024-07-27 11:37:40 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,1 +1,17 @@
-from proc import auth, reward                                     from time import sleep                                            from os import system                                                                                                               while 1:                                                              system("rm -rf res.txt")                                          with open("data.txt", "r") as f:                                      list = f.readlines()                                                                                                            for i in range(len(list)):                                            token = auth(list[i])                                             sleep(0.5)                                                        code = reward(token)                                              sleep(1)                                                          with open("res.txt", "a") as f:                                       f.write(f" acc : {i+1} code : {code}\n")                  sleep(14400)
+from proc import auth, reward
+from time import sleep
+from os import system
+
+while 1:
+    system("rm -rf res.txt")
+    with open("data.txt", "r") as f:
+        list = f.readlines()
+
+    for i in range(len(list)):
+        token = auth(list[i])
+        sleep(0.5)
+        code = reward(token)
+        sleep(1)
+        with open("res.txt", "a") as f:
+            f.write(f" acc : {i+1} code : {code}\n")
+    sleep(14400)
